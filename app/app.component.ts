25d41bc221a7f583f5205fdb5ac7f2c5bb8f6428
zhangkang <zhangkang@lvmama.com>
2017-02-13 11:22:45 +0800

    数据显示

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { Site } from './site';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `<h1>{{title}}</h1>
+  <div *ngFor="let stu of stuList">
+	{{stu}}
+  </div>
+  <!-- 对象输出 -->
+  <li *ngFor="let site of sites">
+	{{site.id}} {{site.name}}
+	<span *ngIf="site.name == 'google'">***</span>
+  </li>
+  <h2>我最喜欢的网站{{mySite}}</h2>
+  `
 })
-export class AppComponent { name = 'Angular2,ss'; }
+export class AppComponent { 
+	title = '站点列表';
+	stuList = ['zk', 'zy', 'zq'];
+	sites = [
+		new Site(1, 'google'),
+		new Site(2, 'baidu'),
+		new Site(3, 'taobao'),
+	];
+	mySite = this.sites[1].name;
+}
