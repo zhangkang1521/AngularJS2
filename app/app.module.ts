e9db7d9353344850f75290edf292f89cae89a6f3
zhangkang <zhangkang@lvmama.com>
2017-02-13 14:42:12 +0800

    clickme2

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { ClickMeComponent } from './click-me.component';
+import { ClickMe2Component } from './click-me2.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent,ClickMeComponent ],
+  declarations: [ AppComponent,
+				  ClickMeComponent,
+				  ClickMe2Component
+				],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
