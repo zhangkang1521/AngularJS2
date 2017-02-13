22209a0922ac6ec38c2824561d232869a7a0ffbc
zhangkang <zhangkang@lvmama.com>
2017-02-13 15:17:29 +0800

    引用多个component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { ClickMeComponent } from './click-me.component';
 import { ClickMe2Component } from './click-me2.component';
+import { KeyUpComponent_v1, KeyUpComponent_v2, KeyUpComponent_v3, KeyUpComponent_v4 } from './keyup.components';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent,
 				  ClickMeComponent,
-				  ClickMe2Component
+				  ClickMe2Component,
+				  KeyUpComponent_v1,
+				  KeyUpComponent_v2,
+				  KeyUpComponent_v3,
+				  KeyUpComponent_v4
 				],
   bootstrap:    [ AppComponent ]
 })
