02daa88fdf238ff324f951f4dcc24666f7cda952
zhangkang <zhangkang@lvmama.com>
2017-02-13 15:46:01 +0800

    loopback

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent }  from './app.component';
 import { ClickMeComponent } from './click-me.component';
 import { ClickMe2Component } from './click-me2.component';
 import { KeyUpComponent_v1, KeyUpComponent_v2, KeyUpComponent_v3, KeyUpComponent_v4 } from './keyup.components';
+import { LoopbackComponent } from './loop-back.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
@@ -13,7 +14,8 @@ import { KeyUpComponent_v1, KeyUpComponent_v2, KeyUpComponent_v3, KeyUpComponent
 				  KeyUpComponent_v1,
 				  KeyUpComponent_v2,
 				  KeyUpComponent_v3,
-				  KeyUpComponent_v4
+				  KeyUpComponent_v4,
+				  LoopbackComponent
 				],
   bootstrap:    [ AppComponent ]
 })
