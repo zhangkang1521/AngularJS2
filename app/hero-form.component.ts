d2e4544e4adfe947ce066a958967d249a8e42000
zhangkang <zhangkang@lvmama.com>
2017-02-14 10:31:54 +0800

    表单

---
+++ app/hero-form.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+
+import { Hero } from './hero';
+
+@Component({
+	moduleId: module.id,
+	selector: 'hero-form',
+	templateUrl: 'hero-form.component.html'
+})
+
+export class HeroFormComponent {
+	powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Change'];
+	model = new Hero(18, 'Andy', this.powers[1], 'Chuck OverStreet');
+	submitted: false;
+	onSubmit() { this.submitted = true; }
+	newHero() {
+		this.model = new Hero(42, '', '')
+	}
+	get diagnostic() { return JSON.stringify(this.model); }
+}
