0bcda588b54ad747a05324114bfade58a8187cd9
zhangkang <zhangkang@lvmama.com>
2017-02-23 11:00:22 +0800

    文件上传插件 http://valor-software.com/ng2-file-upload/

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `<h1>Hello {{name}}</h1><simple-demo></simple-demo>`
 })
 export class AppComponent { name = 'Angular2,ss'; }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import {SimpleDemoComponent} from "./simple-demo.component";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    SimpleDemoComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
